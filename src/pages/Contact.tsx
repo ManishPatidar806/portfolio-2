af6d4eb4ed79478bd1d8191574528627f4d80306
ManishPatidar806 <manishpatidar1180@gmail.com>
2024-03-14 23:39:22 +0530

    update style.css files create project section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import "./Contact.css";
+import logo from "../assets/logoOfContact.png";
+
 
 const Contact: React.FC = () => {
     const [name, setName] = useState<string>("");
@@ -34,30 +37,42 @@ const Contact: React.FC = () => {
     };
 
     return (
-        <main className="min-h-screen flex items-center justify-center flex-col pt-15">
-            <h1 className="m-4 text-5xl font-bold text-center mb-6">Contact</h1>
-            <p className="text-center mb-4">Feel free to reach out to me for any questions or opportunities</p>
-            <div className="p-6 rounded shadow-md max-w-md w-full border">
-                <h2 className="text-2xl font-bold">Email Me</h2>
-                <form className="flex flex-col">
+        <main className='Main'>
+            <div className='Picture-Section' >
+           <img src="../assets/logoOfContact.png" alt="" />
+            </div>
+            <div className='Right-Side'>
+                <p className="Contact">Contact us</p>
+            <div className="Contect-Section">
+                {/* <h2 className="text-2xl font-bold">Email Me</h2> */}
+                <form className="details">
                     <div className="mb-4">
-                        <label className="block mb-2 text-sm font-medium">Name</label>
-                        <input type="text" className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500 text-black caret-red-800" placeholder="Enter Name" value={name} onChange={(e) => { setName(e.target.value) }} />
+                        {/* <label className="block mb-2 text-sm font-medium">Name</label> */}
+                        <input type="text" className="Name-Section" placeholder="  Name" value={name} onChange={(e) => { setName(e.target.value) }} />
                     </div>
                     <div className="mb-4">
-                        <label className="block mb-2 text-sm font-medium">Email</label>
-                        <input type="email" className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500 text-black caret-red-800" placeholder="Enter Email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                        {/* <label className="block mb-2 text-sm font-medium">Email</label> */}
+                        <input type="email" className=" Email-Section" placeholder="Enter Email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
                     </div>
                     <div className="mb-4">
-                        <label className="block mb-2 text-sm font-medium">Message</label>
-                        <textarea className="w-full px-3 py-2 border rounded focus:outline-none text-black caret-red-800" placeholder="Enter Message..." value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
+                        {/* <label className="block mb-2 text-sm font-medium">Message</label> */}
+                        <textarea className="Message-Section" placeholder="Enter Message..." value={message} onChange={(e) => { setMessage(e.target.value) }}></textarea>
                     </div>
-                    <div className="mb-4">
+                    <div className="Submit">
                         <button type="submit" className={`w-full py-2 bg-blue-500 text-white rounded ${btnDisabled ? "opacity-75 cursor-not-allowed" : ""}`} onClick={(e) => { handleForm(e) }} disabled={btnDisabled}>Send Message</button>
                     </div>
                     <p className="text-red-800 font-bold text-center">{response}</p>
                 </form>
+                <div className='Address-Section'>
+                <h3>Contact</h3>
+                <p>manish1880@gmail.com</p>
+                <br></br>
+                <p>CustomerCare:91119154654</p>
+                
+                </div>
             </div>
+            </div>
+            
         </main>
     );
 }
